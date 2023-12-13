e2683626d149094ca838c0a720dfe2312f1757fe
Luca Octavian <octavian.luca@student.unitbv.ro>
2023-12-13 19:40:59 +0200

    Incercare ui 1

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -3,7 +3,7 @@ import webview
 import socket
 import sys
 import threading
-import pyi_splash
+#import pyi_splash
 
 with socket.socket() as s:
     s.bind(('',0))
@@ -11,7 +11,16 @@ with socket.socket() as s:
 
 #NiceGUI 
 ui.label('Hello NiceGUI!')
-ui.button('Click me', on_click=lambda: ui.notify('Button clicked!'))
-app.on_startup(lambda: pyi_splash.close())
+ui.chat_message('Hi',
+                name='Bianca',
+                stamp='now',
+                avatar='https://robohash.org/ui')
+
+ui.chat_message('Hello there!',
+                name='Capitanul Pitonescu',
+                stamp='now',
+                avatar='https://robohash.org/ui')
+
+#app.on_startup(lambda: pyi_splash.close())
 
 ui.run(native=True, reload=False, port=openport)
