fa4095b21d6949980a736878f041b6e87c4e2da4
Andrei Nedelcu <andreinedelcu48@gmail.com>
2023-12-13 17:31:49 +0200

    adaugare modul AI

---
+++ src/ai/chat.py
@@ -0,0 +1,5 @@
+from chatterbot import ChatBot
+chatbot = ChatBot('Capitanul Pitonescu')
+
+response = chatbot.get_response("intrebare")
+print(response)
--- src/gui/gui.py
+++ src/gui/gui.py
@@ -3,7 +3,7 @@ import webview
 import socket
 import sys
 import threading
-import pyi_splash
+#import pyi_splash
 
 with socket.socket() as s:
     s.bind(('',0))
@@ -12,6 +12,7 @@ with socket.socket() as s:
 #NiceGUI 
 ui.label('Hello NiceGUI!')
 ui.button('Click me', on_click=lambda: ui.notify('Button clicked!'))
-app.on_startup(lambda: pyi_splash.close())
+
+#app.on_startup(lambda: pyi_splash.close())
 
 ui.run(native=True, reload=False, port=openport)
