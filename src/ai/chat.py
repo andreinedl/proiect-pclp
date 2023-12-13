b9872042717a650f9c7ae2295f08838620fbd9b9
Eduard Lupsa <eduard.lupsa@student.unitbv.ro>
2023-12-13 19:34:02 +0200

    folosirea modelelor de catre Bot

--- src/ai/chat.py
+++ src/ai/chat.py
@@ -1,6 +1,19 @@
-from chatterbot import ChatBot, storage
-from chatterbot.trainers import ListTrainer
+from chatterbot import ChatBot, storage, corpus
+from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
+from pathlib import Path, PurePosixPath
+import os
 chatbot = ChatBot('Capitanul Pitonescu')
-trainer = ListTrainer(chatbot)
-response = chatbot.get_response("intrebare")
-print(response)
+trainer = ChatterBotCorpusTrainer(chatbot)
+
+enModels = str(PurePosixPath(Path(os.getcwd()))) + "/src/ai/models/en/"
+
+trainer.train(
+   enModels
+)
+
+while True:
+    intrebare = input("Intrebare: ")
+    response = chatbot.get_response(intrebare)
+    print(response)
+    
+#doTrain()
