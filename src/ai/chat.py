62c3221512be2c0b3d29584ea6d7df0ce96bae94
Eduard Lupsa <eduard.lupsa@student.unitbv.ro>
2023-12-13 18:09:35 +0200

    importat ListTrainer+ training chatAI

--- src/ai/chat.py
+++ src/ai/chat.py
@@ -1,5 +1,6 @@
-from chatterbot import ChatBot
+from chatterbot import ChatBot, storage
+from chatterbot.trainers import ListTrainer
 chatbot = ChatBot('Capitanul Pitonescu')
-
+trainer = ListTrainer(chatbot)
 response = chatbot.get_response("intrebare")
 print(response)
