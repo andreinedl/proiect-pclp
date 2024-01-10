97e4f730313731a7470cdab8d04519dfb4d4a0b6
Andrei Nedelcu <andreinedelcu48@gmail.com>
2024-01-10 23:35:01 +0200

    Restructurare GUI, optimizare cod

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
-from gui import gui
+from gui import gui as gui
 import spacy
 if(spacy.util.is_package("en_core_web_sm") == False):
     spacy.cli.download("en_core_web_sm")
+    
+if(spacy.util.is_package("ro_core_news_sm") == False):
+    spacy.cli.download("ro_core_news_sm")
     
 if __name__ == '__main__':
     gui.start()
