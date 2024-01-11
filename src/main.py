ac66b9c44961f506a2c4dbc83642e7f620de2554
Andrei Nedelcu <andreinedelcu48@gmail.com>
2024-01-11 17:34:22 +0200

    adaugare mesaj initial

--- src/main.py
+++ src/main.py
@@ -1,10 +1,4 @@
-from gui import gui as gui
-import spacy
-if(spacy.util.is_package("en_core_web_sm") == False):
-    spacy.cli.download("en_core_web_sm")
-    
-if(spacy.util.is_package("ro_core_news_sm") == False):
-    spacy.cli.download("ro_core_news_sm")
+from gui import gui
     
 if __name__ == '__main__':
     gui.start()
